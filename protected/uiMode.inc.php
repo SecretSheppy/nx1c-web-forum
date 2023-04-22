84c0cd2a271cbfd9ebe62a3acbf0aa12e88ed8ce
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2023-04-22 10:20:20 +0100

    Theme Support
    Custom theme colors now used

--- protected/uiMode.inc.php
+++ protected/uiMode.inc.php
@@ -3,3 +3,7 @@
 if ($_SESSION["user"]["uiMode"] == 1) {
     echo '<link rel="stylesheet" href="resources/css/main.dark.css">';
 }
+
+if ($_SESSION["user"]["theme"] != "") {
+    echo '<style>:root{--theme-bgcolor: ' . $_SESSION["user"]["theme"] . ';}</style>';
+}
