824f47800692730fd0022aa7f3a3e33d773be9f6
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2023-05-04 11:22:19 +0100

    heredoc html rendering

--- nx1c.php
+++ nx1c.php
@@ -3,15 +3,12 @@
 session_start();
 
 include 'tools/load_settings.inc.php';
-
 include 'tools/gate_keeper.php';
 gate_keeper($_SESSION["nx1c-installation-settings"]->settings->captcha);
 
 include 'protected/db.inc.php';
 include 'tools/text_formatting.php';
 
-// TODO https://stackoverflow.com/questions/7537377/how-to-include-a-php-variable-inside-a-mysql-statement
-// TODO using protected/sql/search.sql
 
 if (isset($_GET["search-value"])) {
     $sql = "SELECT posts.postid, posts.title, posts.content, posts.tags, posts.likes, users.name, users.role, users.userid FROM posts, users WHERE posts.userid = users.userid AND posts.content LIKE '%" . $_GET["search-value"] . "%' ORDER BY posts.postid DESC LIMIT 30";
@@ -66,49 +63,59 @@ include 'tools/subnav.inc.php';
         <?php
 
         if (isset($_SESSION["user"])) {
-            echo '<div class="item" style="min-height: 0; height: fit-content;">
-                    <div class="inner" style="padding: 30px 20px 30px 20px;">
-                        <a href="compose.php" class="theme">Write A Post</a>
-                        <a href="nx1c.php?userid=' . $_SESSION["user"]["id"] . '">See My Posts</a>
-                    </div>
-                </div>';
+            echo <<< HTML
+            <div class="item" style="min-height: 0; height: fit-content;">
+                <div class="inner" style="padding: 30px 20px 30px 20px;">
+                    <a href="compose.php" class="theme">Write A Post</a>
+                    <a href="nx1c.php?userid={$_SESSION["user"]["id"]}">See My Posts</a>
+                </div>
+            </div>
+            HTML;
         }
 
         if ($result->num_rows > 0) {
             for ($i = 0; $i < $result->num_rows; $i++) {
+
                 $row = $result->fetch_assoc();
-                echo '<div class="item">
-                        <div class="inner">
-                            <div class="username-container">
-                                <a class="username-display" href="nx1c.php?userid=' . $row["userid"] . '" id="' . $row["role"] . '"><strong>' . $row["name"] . '</strong></a>
-                                <div class="username-container-right">
-                                    <p><strong>' . $row["likes"] . '</strong> Likes</p>
-                                </div>
-                            </div>
-                            <h1>' . $row["title"] . '</h1>
-                            <div class="tags">
-                                ';
+                $paragraph_data = print_paragraph($row["content"]);
+                $tags_data = "";
 
                 if ($row["tags"] !== null) {
-                    $tags = explode(", ", $row["tags"]);
-
-                    foreach ($tags as $tag) {
-                        echo '<a class="tag" href="nx1c.php?tag=' . $tag . '">' . $tag . '</a>';
+                    foreach (explode(", ", $row["tags"]) as $tag) {
+                        $tags_data .= '<a class="tag" href="nx1c.php?tag=' . $tag . '">' . $tag . '</a>';
                     }
                 }
 
-                echo '</div>';
-
-                echo print_paragraph($row["content"]);
-
-                echo '</div>
-                        <div class="center">
-                            <a href="post.php?postid=' . $row["postid"] . '">See Full Post</a>
+                echo <<< HTML
+                <div class="item">
+                    <div class="inner">
+                        <div class="username-container">
+                            <a class="username-display" href="nx1c.php?userid={$row["userid"]}" id="{$row["role"]}"><strong>{$row["name"]}</strong></a>
+                            <div class="username-container-right">
+                                <p><strong>{$row["likes"]}</strong> Likes</p>
+                            </div>
                         </div>
-                    </div>';
+                        <h1>{$row["title"]}</h1>
+                        <div class="tags">
+                        {$tags_data}
+                        </div>
+                        {$paragraph_data}
+                    </div>
+                    <div class="center">
+                        <a href="post.php?postid={$row["postid"]}">See Full Post</a>
+                    </div>
+                </div>
+                HTML;
             }
         } else {
-            echo "<p>No Posts Found</p>";
+            echo <<< HTML
+            <div class="item">
+                <div class="inner">
+                    <h1>Sorry, No posts found :(</h1>
+                    <p>No posts could be found for the user or search query you made.</p>
+                </div>
+            </div>
+            HTML;
         }
         ?>
     </div>
@@ -117,26 +124,30 @@ include 'tools/subnav.inc.php';
             <?php
 
             if (!isset($_SESSION["user"])) {
-                echo '<div class="login-banner">
-                        <div class="lb-inner">
-                            <h3>Login to see highest rated users</h3>
-                            <br>
-                            <a href="login.php" class="theme">Login</a>
-                        </div>
-                    </div>';
+                echo <<< HTML
+                <div class="login-banner">
+                    <div class="lb-inner">
+                        <h3>Login to see the highest rated users</h3>
+                        <br>
+                        <a href="login.php" class="theme">Login</a>
+                    </div>
+                </div>
+                HTML;
             } else {
 
                 $sql = "SELECT userid, name, upvotes FROM users ORDER BY upvotes DESC LIMIT 8";
                 $result = $db->query($sql);
                 for ($i = 0; $i < $result->num_rows; $i++) {
                     $row = $result->fetch_assoc();
-                    echo '<div class="user">
-                            <div>
-                                <h3>' . $row["name"] . '</h3> 
-                                <p>' . $row["upvotes"] . ' Likes</p>                    
-                            </div>
-                            <a href="nx1c.php?userid=' . $row["userid"] . '">View</a>
-                        </div>';
+                    echo <<< HTML
+                    <div class="user">
+                        <div>
+                            <h3>{$row["name"]}</h3> 
+                            <p>{$row["upvotes"]} Likes</p>                    
+                        </div>
+                        <a href="nx1c.php?userid={$row["userid"]}">View</a>
+                    </div>
+                    HTML;
                 }
 
             }
