63c7bf43dbb2ca72a0c8ed7cbcedda6d9def25d2
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2023-05-15 09:22:42 +0100

    push before bug fixing

--- signup.php
+++ signup.php
@@ -8,6 +8,11 @@ gate_keeper(1, false);
 include 'protected/db.inc.php';
 require('tools/generate_security_token.php');
 
+$error = (string) filter_input(INPUT_GET, "error", FILTER_DEFAULT);
+$username = (string) filter_input(INPUT_POST, "username", FILTER_SANITIZE_ADD_SLASHES);
+$password = (string) filter_input(INPUT_POST, "password", FILTER_SANITIZE_ADD_SLASHES);
+$password_confirm = (string) filter_input(INPUT_POST, "confirm-password", FILTER_SANITIZE_ADD_SLASHES);
+
 ?>
 
 <!DOCTYPE html>
@@ -21,19 +26,19 @@ require('tools/generate_security_token.php');
 <body>
 <?php
 
-if (isset($error)) {
-    echo '<div class="alert">
-            <p>An error occurred during the signup process, please try again later!</p>
-        </div>';
+if ($error != null) {
+    echo <<< HTML
+    <div class="alert">
+        <p>Error: {$error}</p>
+    </div>
+    HTML;
 }
 
 ?>
 <div class="login-wrapper">
     <?php
 
-    if (isset($_POST["username"])) {
-
-        $username = $_POST["username"];
+    if ($username != null) {
 
         $sql = "SELECT * FROM users WHERE name = '$username'";
         $result = $db->query($sql);
@@ -41,63 +46,74 @@ if (isset($error)) {
         // only create account if account with that name doesn't exist
         if ($result->num_rows == 0) {
 
-            $password = $_POST["password"];
+            if ($password != $password_confirm) {
+                header("Location: signup.php?error=Passwords+did+not+match!");
+                exit();
+            }
+
             // TODO check passwords match
             $token = generate_security_token("abcdefghijklmnopqrstuvwxyz", 50);
 
             $sql = "INSERT INTO users (name, password, securityToken) VALUES ('$username', '$password', '$token')";
 
             if ($db->query($sql) === TRUE) {
-                echo '<div class="login">
-                        <h1>NX1C</h1>
-                        <p>One Last Thing</p>
-                        <br>
-                        <p>
-                            A random security token has been generated for your account. You will need this in order to reset your password or delete your account.
-                            Do not lose or share it with anyone!
-                        </p>
-                        <br>
-                        <p>Your Security Token</p>
-                        <div class="token-wrapper">
-                            <p>' . $token . '</p>
-                        </div>
-                        <br>
-                        <div class="spacer">
-                            <a href="login.php" class="link">I have saved my security token</a>
-                        </div>
-                    </div>';
+                echo <<< HTML
+                <div class="login">
+                    <h1>NX1C</h1>
+                    <p>One Last Thing</p>
+                    <br>
+                    <p>
+                        A random security token has been generated for your account. You will need this in order to reset your password or delete your account.
+                        Do not lose or share it with anyone!
+                    </p>
+                    <br>
+                    <p>Your Security Token</p>
+                    <div class="token-wrapper">
+                        <p>{$token}</p>
+                    </div>
+                    <br>
+                    <div class="spacer">
+                        <a href="login.php" class="link">I have saved my security token</a>
+                    </div>
+                </div>
+                HTML;
             } else {
                 header("Location: err.php");
                 exit();
             }
 
+        } else {
+            header("Location: signup.php?error=Username+already+taken!");
+            exit();
         }
     } else {
-        echo '<div class="login">
-                <h1>NX1C</h1>
-                <p>User Sign Up</p>
-                <form method="POST" enctype="application/x-www-form-urlencoded">
-                    <div class="label-wrapper">
-                        <label for="username">Username</label>
-                    </div>
-                    <input id="username" type="text" placeholder="Enter your username" name="username" autocomplete="off" required />
-                    <div class="label-wrapper">
-                        <label for="password">Password</label>
-                    </div>
-                    <input id="password" type="password" placeholder="Enter your password" name="password" autocomplete="off" required />
-                    <div class="label-wrapper">
-                        <label for="confirm-password">Confirm Password</label>
-                    </div>
-                    <input id="confirm-password" type="password" placeholder="Enter your password again" name="confirm-password" autocomplete="off" required />
-                    <div class="button-wrapper">
-                        <input type="reset" value="Cancel" class="spacing" />
-                        <input type="submit" value="Sign Up" />
-                    </div>
-                </form>
-                <div class="spacer">
-                    <a href="nx1c.php" class="link">Return to home page</a>
+        echo <<< HTML
+        <div class="login">
+            <h1>NX1C</h1>
+            <p>User Sign Up</p>
+            <form method="POST" enctype="application/x-www-form-urlencoded">
+                <div class="label-wrapper">
+                    <label for="username">Username</label>
                 </div>
-            </div>';
+                <input id="username" type="text" placeholder="Enter your username" name="username" autocomplete="off" required />
+                <div class="label-wrapper">
+                    <label for="password">Password</label>
+                </div>
+                <input id="password" type="password" placeholder="Enter your password" name="password" autocomplete="off" required />
+                <div class="label-wrapper">
+                    <label for="confirm-password">Confirm Password</label>
+                </div>
+                <input id="confirm-password" type="password" placeholder="Enter your password again" name="confirm-password" autocomplete="off" required />
+                <div class="button-wrapper">
+                    <input type="reset" value="Cancel" class="spacing" />
+                    <input type="submit" value="Sign Up" />
+                </div>
+            </form>
+            <div class="spacer">
+                <a href="nx1c.php" class="link">Return to home page</a>
+            </div>
+        </div>
+        HTML;
     }
 
     ?>
