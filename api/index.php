4a4a4f0a7e619cdbf434988ed67a6cb72eb4c419
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2023-05-16 23:05:40 +0100

    Initial API Commit

---
+++ api/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include '../protected/db.inc.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+// https://stackoverflow.com/questions/4064444/returning-json-from-a-php-script
+// reference link
+
+// TODO - this will be the nx1c api
